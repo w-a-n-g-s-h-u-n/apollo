c83e6ab6ddf861e601b01a324e2507dada0713d2
汪顺 <wangshun@genomics.cn>
2018-11-27 21:19:37 +0800

    自动配置

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/boot/ApolloAutoConfiguration.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/boot/ApolloAutoConfiguration.java
@@ -1,15 +1,18 @@
 package com.ctrip.framework.apollo.spring.boot;
 
-import com.ctrip.framework.apollo.spring.config.ConfigPropertySourcesProcessor;
-import com.ctrip.framework.apollo.spring.config.PropertySourcesConstants;
-import com.ctrip.framework.apollo.spring.config.PropertySourcesProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import com.ctrip.framework.apollo.spring.config.ConfigPropertySourcesProcessor;
+import com.ctrip.framework.apollo.spring.config.PropertySourcesConstants;
+import com.ctrip.framework.apollo.spring.config.PropertySourcesProcessor;
+
 @Configuration
 @ConditionalOnProperty(PropertySourcesConstants.APOLLO_BOOTSTRAP_ENABLED)
+@EnableApolloConfig
 @ConditionalOnMissingBean(PropertySourcesProcessor.class)
 public class ApolloAutoConfiguration {
 
