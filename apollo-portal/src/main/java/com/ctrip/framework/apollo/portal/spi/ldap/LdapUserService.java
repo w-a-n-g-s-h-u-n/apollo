29d7a520f38d6c4da7e2b104b426cb5e2455358a
汪顺 <wangshun@genomics.cn>
2018-12-17 20:29:46 +0800

    限制可登录用户

--- apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/spi/ldap/LdapUserService.java
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/spi/ldap/LdapUserService.java
@@ -2,12 +2,9 @@ package com.ctrip.framework.apollo.portal.spi.ldap;
 
 import static org.springframework.ldap.query.LdapQueryBuilder.query;
 
-import com.ctrip.framework.apollo.core.utils.StringUtils;
-import com.ctrip.framework.apollo.portal.entity.bo.UserInfo;
-import com.ctrip.framework.apollo.portal.spi.UserService;
-import com.google.common.base.Strings;
 import java.util.Arrays;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ldap.core.ContextMapper;
@@ -17,6 +14,11 @@ import org.springframework.ldap.query.ContainerCriteria;
 import org.springframework.ldap.query.SearchScope;
 import org.springframework.util.CollectionUtils;
 
+import com.ctrip.framework.apollo.core.utils.StringUtils;
+import com.ctrip.framework.apollo.portal.entity.bo.UserInfo;
+import com.ctrip.framework.apollo.portal.spi.UserService;
+import com.google.common.base.Strings;
+
 /**
  * @author xm.lin xm.lin@anxincloud.com
  * @Description
@@ -24,71 +26,80 @@ import org.springframework.util.CollectionUtils;
  */
 public class LdapUserService implements UserService {
 
-  @Value("${ldap.mapping.objectClass}")
-  private String objectClassAttrName;
-  @Value("${ldap.mapping.loginId}")
-  private String loginIdAttrName;
-  @Value("${ldap.mapping.userDisplayName}")
-  private String userDisplayNameAttrName;
-  @Value("${ldap.mapping.email}")
-  private String emailAttrName;
-  @Value("#{'${ldap.filter.memberOf:}'.split('\\|')}")
-  private String[] memberOf;
+    @Value("${ldap.mapping.objectClass}")
+    private String objectClassAttrName;
+    @Value("${ldap.mapping.loginId}")
+    private String loginIdAttrName;
+    @Value("${ldap.mapping.userDisplayName}")
+    private String userDisplayNameAttrName;
+    @Value("${ldap.mapping.email}")
+    private String emailAttrName;
+    @Value("#{'${ldap.filter.memberOf:}'.split('\\|')}")
+    private String[] memberOf;
+    @Value("#{'${ldap.filter.department:}'.split('\\|')}")
+    private String[] department;
+    @Value("#{'${ldap.filter.division:}'.split('\\|')}")
+    private String[] division;
 
-  @Autowired
-  private LdapTemplate ldapTemplate;
+    @Autowired
+    private LdapTemplate ldapTemplate;
 
-  private static final String MEMBER_OF_ATTR_NAME = "memberOf";
+    private static final String MEMBER_OF_ATTR_NAME = "memberOf";
+    private static final String DEPARTMENT_ATTR_NAME = "department";
+    private static final String DIVISION_ATTR_NAME = "division";
 
-  private ContextMapper<UserInfo> ldapUserInfoMapper = (ctx) -> {
-    DirContextAdapter contextAdapter = (DirContextAdapter) ctx;
-    UserInfo userInfo = new UserInfo();
-    userInfo.setUserId(contextAdapter.getStringAttribute(loginIdAttrName));
-    userInfo.setName(contextAdapter.getStringAttribute(userDisplayNameAttrName));
-    userInfo.setEmail(contextAdapter.getStringAttribute(emailAttrName));
-    return userInfo;
-  };
+    private ContextMapper<UserInfo> ldapUserInfoMapper = (ctx) -> {
+        DirContextAdapter contextAdapter = (DirContextAdapter)ctx;
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserId(contextAdapter.getStringAttribute(loginIdAttrName));
+        userInfo.setName(contextAdapter.getStringAttribute(userDisplayNameAttrName));
+        userInfo.setEmail(contextAdapter.getStringAttribute(emailAttrName));
+        return userInfo;
+    };
 
-  private ContainerCriteria ldapQueryCriteria() {
-    ContainerCriteria criteria = query()
-        .searchScope(SearchScope.SUBTREE)
-        .where("objectClass").is(objectClassAttrName);
-    if (memberOf.length > 0 && !StringUtils.isEmpty(memberOf[0])) {
-      ContainerCriteria memberOfFilters = query().where(MEMBER_OF_ATTR_NAME).is(memberOf[0]);
-      Arrays.stream(memberOf).skip(1)
-          .forEach(filter -> memberOfFilters.or(MEMBER_OF_ATTR_NAME).is(filter));
-      criteria.and(memberOfFilters);
+    private ContainerCriteria ldapQueryCriteria() {
+        ContainerCriteria criteria = query().searchScope(SearchScope.SUBTREE).where("objectClass").is(objectClassAttrName);
+        if (memberOf.length > 0 && !StringUtils.isEmpty(memberOf[0])) {
+            ContainerCriteria memberOfFilters = query().where(MEMBER_OF_ATTR_NAME).is(memberOf[0]);
+            Arrays.stream(memberOf).skip(1).forEach(filter -> memberOfFilters.or(MEMBER_OF_ATTR_NAME).is(filter));
+            criteria.and(memberOfFilters);
+        }
+        if (department.length > 0 && !StringUtils.isEmpty(department[0])) {
+            ContainerCriteria departmentFilters = query().where(DEPARTMENT_ATTR_NAME).is(department[0]);
+            Arrays.stream(department).skip(1).forEach(filter -> departmentFilters.or(DEPARTMENT_ATTR_NAME).is(filter));
+            criteria.and(departmentFilters);
+        }
+        if (division.length > 0 && !StringUtils.isEmpty(division[0])) {
+            ContainerCriteria divisionFilters = query().where(DIVISION_ATTR_NAME).is(division[0]);
+            Arrays.stream(division).skip(1).forEach(filter -> divisionFilters.or(DIVISION_ATTR_NAME).is(filter));
+            criteria.and(divisionFilters);
+        }
+        return criteria;
     }
-    return criteria;
-  }
 
-
-  @Override
-  public List<UserInfo> searchUsers(String keyword, int offset, int limit) {
-    ContainerCriteria criteria = ldapQueryCriteria();
-    if (!Strings.isNullOrEmpty(keyword)) {
-      criteria.and(query().where(loginIdAttrName).like(keyword + "*").or(userDisplayNameAttrName)
-          .like(keyword + "*"));
+    @Override
+    public List<UserInfo> searchUsers(String keyword, int offset, int limit) {
+        ContainerCriteria criteria = ldapQueryCriteria();
+        if (!Strings.isNullOrEmpty(keyword)) {
+            criteria.and(query().where(loginIdAttrName).like(keyword + "*").or(userDisplayNameAttrName).like(keyword + "*"));
+        }
+        return ldapTemplate.search(criteria, ldapUserInfoMapper);
     }
-    return ldapTemplate.search(criteria, ldapUserInfoMapper);
-  }
 
-  @Override
-  public UserInfo findByUserId(String userId) {
-    return ldapTemplate
-        .searchForObject(ldapQueryCriteria().and(loginIdAttrName).is(userId), ldapUserInfoMapper);
-  }
-
-  @Override
-  public List<UserInfo> findByUserIds(List<String> userIds) {
-    if (CollectionUtils.isEmpty(userIds)) {
-      return null;
-    } else {
-      ContainerCriteria criteria = ldapQueryCriteria()
-          .and(query().where(loginIdAttrName).is(userIds.get(0)));
-      userIds.stream().skip(1).forEach(userId -> criteria.or(loginIdAttrName).is(userId));
-      return ldapTemplate.search(criteria, ldapUserInfoMapper);
+    @Override
+    public UserInfo findByUserId(String userId) {
+        return ldapTemplate.searchForObject(ldapQueryCriteria().and(loginIdAttrName).is(userId), ldapUserInfoMapper);
     }
-  }
+
+    @Override
+    public List<UserInfo> findByUserIds(List<String> userIds) {
+        if (CollectionUtils.isEmpty(userIds)) {
+            return null;
+        } else {
+            ContainerCriteria criteria = ldapQueryCriteria().and(query().where(loginIdAttrName).is(userIds.get(0)));
+            userIds.stream().skip(1).forEach(userId -> criteria.or(loginIdAttrName).is(userId));
+            return ldapTemplate.search(criteria, ldapUserInfoMapper);
+        }
+    }
 
 }
